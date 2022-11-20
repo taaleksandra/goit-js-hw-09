7156e115b798ea360b2578d236b03ff2dbf761e6
taaleksandra <olatarnas@gmail.com>
2022-11-20 19:24:15 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+const colorChanger = () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  console.log('Current body background color: ', body.style.backgroundColor);
+  timerId = setInterval(() => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+    console.log('Current body background color: ', body.style.backgroundColor);
+  }, 1000);
+};
+
+const breakColorChanger = () => {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timerId);
+};
+
+stopBtn.disabled = true;
+startBtn.addEventListener('click', colorChanger);
+stopBtn.addEventListener('click', breakColorChanger);
