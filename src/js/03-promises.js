720962d4b26cbd987a01015ca5b6812716ccf8b7
taaleksandra <olatarnas@gmail.com>
2022-12-01 22:07:07 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,50 +3,44 @@
 import Notiflix from 'notiflix';
 
 const form = document.querySelector('form');
-const btnCreate = document.querySelector('button');
+const btnCreate = document.querySelector('.form button');
 
-// let promiseNumber = 1;
-// let data = {
-//   position: '',
-//   delay: '',
-// };
-let timerId = null;
+let promiseNumber = 1;
+let delayPromise = 0;
 
-function createPromise({ position, delay }) {
+function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
 
-  new Promise((resolve, reject) => {
-    data = {
-      position: position,
-      delay: delay,
-    };
-    if (shouldResolve) {
-      // Fulfill
-      resolve({ position, delay });
-    } else {
-      // Reject
-      reject({ position, delay });
-    }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(position, delay);
+      } else {
+        // Reject
+        reject(position, delay);
+      }
+    }, delay);
   });
 }
 
-const renderMessage = ({ position, delay }) => {
-  createPromise({ position, delay })
+const renderMessage = (position, delay) => {
+  createPromise(position, delay)
     .then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
     })
     .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
     });
 };
 
 const btnHandler = event => {
   event.preventDefault();
-  const {
-    elements: { delay, step, amount },
-  } = form;
-  for (let i = 0; i < amount.value; i++) {
-    const promiseNumber = i + 1;
-    renderMessage({ promiseNumber, delay });
+  for (let i = 0; i < form.elements.amount.value; i++) {
+    promiseNumber = i + 1;
+    delayPromise = form.elements.delay.value + form.elements.step.value * i;
+    renderMessage(promiseNumber, delayPromise);
   }
 };
+
+form.addEventListener('submit', btnHandler);
